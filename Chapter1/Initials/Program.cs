533c3d0f20d4187d48418c73b996757fd9ed06ca
duggank <CreativeWeep@gmail.com>
2014-09-05 08:38:06 -0400

    Final Commit
    We added Quotation, Tree, and Initials. We finished everything! :D

---
+++ Chapter1/Initials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("K  K    DDD");
+            Console.WriteLine("K K     D  D");
+            Console.WriteLine("KK      D   D");
+            Console.WriteLine("K K     D   D");
+            Console.WriteLine("K  K    D  D");
+            Console.WriteLine("K   K   DDD");
+        }
+    }
+}
